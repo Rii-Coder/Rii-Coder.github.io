5fe638d29fe569128f9f9514cea75a52f2f37f9f
Rii-Coder <63327126+Rii-Coder@users.noreply.github.com>
2022-09-04 12:30:07 +0000

    Deploying to public from @ Rii-Coder/Rii-Coder.github.io@4312da988ff9c8b7ba89905b31c9b7e6ee7db800 🚀

---
+++ _next/static/zkVE66b-HVFK7E-NjX-nz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-e1a4727575ad540d.js","static/css/8bcb955bb4c02423.css","static/chunks/pages/index-f14e1d14257d888a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
