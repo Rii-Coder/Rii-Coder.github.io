6f4df0fc737d21a4a66fb5f4b1098919df33d51b
TheR2D2cx <jamv1305@gmail.com>
2022-09-04 05:08:32 -0700

    revert 2

--- Code/.next/static/yugT4YzbfnrOkpIOQoz9p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-1fa8ae2c7b05bdcf.js","static/css/8bcb955bb4c02423.css","static/chunks/pages/index-0a7929a0d7be4e22.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
